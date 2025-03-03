1d984f72ef4f2060cd03cc9b6057e87aa284f886
David Nuno Fernandes Roque <david-fe@c2r2s3.42porto.com>
2025-03-03 17:12:57 +0000

    made changes to struct and main

--- fractol.h
+++ fractol.h
@@ -14,11 +14,21 @@
 # define FRACTOL_H
 # include "mlx-linux/mlx.h"
 # include <X11/X.h>
+# include <stdlib.h>
 
 typedef struct s_coord
 {
 	int x;
 	int y;
 } t_fractal;
+
+typedef struct	s_img {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+} t_img;
+
 
 #endif
--- src/main.c
+++ src/main.c
@@ -12,12 +12,39 @@
 
 #include "../fractol.h"
 
+void	mlx_put_pixel(t_img *img, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
 int	main(void)
 {
 	void	*mlx;
 	void	*mlx_win;
-	
+	t_img	img;
+	int		offset; //incomplete
+
 	mlx = mlx_init();
-	mlx_win =  mlx_new_window(mlx, 500, 500, "test window");
+	if(!mlx)
+		return(1);
+
+	img.img = mlx_new_image(mlx, 1600, 800);
+	img.addr = mlx_get_data_addr(
+		img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+	mlx_win =  mlx_new_window(mlx, 1600, 800, "test window");
+
+	if(!mlx_win)
+	{
+		mlx_destroy_display(mlx_win);
+		free(mlx);
+		return(1);
+	}
+
 	mlx_loop(mlx);
+//
+	mlx_destroy_display(mlx_win);
+	free(mlx);
 }

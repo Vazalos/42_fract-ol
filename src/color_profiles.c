f957c8ebf84279a5e2cc45b72b212dbdbb5f702e
David Nuno Fernandes Roque <david-fe@c1r6s4.42porto.com>
2025-03-27 17:18:30 +0000

    color profiles complete

--- src/color_profiles.c
+++ src/color_profiles.c
@@ -85,52 +85,53 @@ int	ft_3rd_color(int i, t_data *mlx)
 	d = mlx->max_iter;
 	if (i >= d - mlx->j)
 		color = ft_encode_argb(255,
-				ft_map(i, ft_range(d - mlx->j, d), ft_range(240, 240)),
-				ft_map(i, ft_range(d - mlx->j, d), ft_range(240, 40)),
-				ft_map(i, ft_range(d - mlx->j, d), ft_range(30, 50)));
+				ft_map(i, ft_range(d - mlx->j, d), ft_range(255, 255)),
+				ft_map(i, ft_range(d - mlx->j, d), ft_range(127, 215)),
+				ft_map(i, ft_range(d - mlx->j, d), ft_range(80, 0)));
 	if (i < d - mlx->j && i >= d - mlx->k)
 		color = ft_encode_argb(255,
-				ft_map(i, ft_range(d - mlx->k, d - mlx->j), ft_range(240, 240)),
-				ft_map(i, ft_range(d - mlx->k, d - mlx->j), ft_range(240, 240)),
-				ft_map(i, ft_range(d - mlx->k, d - mlx->j), ft_range(30, 30)));
-	if (i < d - mlx->l && i >= d - mlx->l)
+				ft_map(i, ft_range(d - mlx->k, d - mlx->j), ft_range(255, 255)),
+				ft_map(i, ft_range(d - mlx->k, d - mlx->j), ft_range(20, 127)),
+				ft_map(i, ft_range(d - mlx->k, d - mlx->j), ft_range(147, 80)));
+	if (i < d - mlx->k && i >= d - mlx->l)
 		color = ft_encode_argb(255,
-				ft_map(i, ft_range(d - mlx->l, d - mlx->k), ft_range(30, 240)),
-				ft_map(i, ft_range(d - mlx->l, d - mlx->k), ft_range(255, 240)),
-				ft_map(i, ft_range(d - mlx->l, d - mlx->k), ft_range(50, 30)));
+				ft_map(i, ft_range(d - mlx->l, d - mlx->k), ft_range(128, 255)),
+				ft_map(i, ft_range(d - mlx->l, d - mlx->k), ft_range(0, 20)),
+				ft_map(i, ft_range(d - mlx->l, d - mlx->k), ft_range(128, 147)));
 	if (i < d - mlx->l)
 		color = ft_encode_argb(255,
-				ft_map(i, ft_range(0, d - mlx->l), ft_range(10, 30)),
-				ft_map(i, ft_range(0, d - mlx->l), ft_range(0, 255)),
-				ft_map(i, ft_range(0, d - mlx->l), ft_range(10, 50)));
+				ft_map(i, ft_range(0, d - mlx->l), ft_range(0, 128)),
+				ft_map(i, ft_range(0, d - mlx->l), ft_range(0, 0)),
+				ft_map(i, ft_range(0, d - mlx->l), ft_range(0, 128)));
 	return (color);
 }
 
 int	ft_4th_color(int i, t_data *mlx)
 {
 	int	color;
-	double max;
-	double n;
-	double m;
+	double d;
 
-	max = mlx->max_iter;
-	n = max/8;
-	m = 4;
-	if (i >= max - n)
+	d = mlx->max_iter;
+	if (i >= d - mlx->j)
 		color = ft_encode_argb(255,
-				ft_map(i, ft_range(max - n, max), ft_range(200, 250)),
-				ft_map(i, ft_range(max - n, max), ft_range(200, 30)),
-				ft_map(i, ft_range(max - n, max), ft_range(50, 40)));
-	if (i < max - n && i >= max - (n * m))
+				ft_map(i, ft_range(d - mlx->j, d), ft_range(230, 240)),
+				ft_map(i, ft_range(d - mlx->j, d), ft_range(200, 40)),
+				ft_map(i, ft_range(d - mlx->j, d), ft_range(45, 50)));
+	if (i < d - mlx->j && i >= d - mlx->k)
 		color = ft_encode_argb(255,
-				ft_map(i, ft_range(max - (n * m), max - n), ft_range(50, 200)),
-				ft_map(i, ft_range(max - (n * m), max - n), ft_range(255, 200)),
-				ft_map(i, ft_range(max - (n * m), max - n), ft_range(70, 50)));
-	if (i < max - (n * m))
+				ft_map(i, ft_range(d - mlx->k, d - mlx->j), ft_range(220, 230)),
+				ft_map(i, ft_range(d - mlx->k, d - mlx->j), ft_range(250, 200)),
+				ft_map(i, ft_range(d - mlx->k, d - mlx->j), ft_range(40, 45)));
+	if (i < d - mlx->k && i >= d - mlx->l)
 		color = ft_encode_argb(255,
-				ft_map(i, ft_range(0, max - (n * m)), ft_range(10, 50)),
-				ft_map(i, ft_range(0, max - (n * m)), ft_range(0, 255)),
-				ft_map(i, ft_range(0, max - (n * m)), ft_range(10, 70)));
+				ft_map(i, ft_range(d - mlx->l, d - mlx->k), ft_range(30, 220)),
+				ft_map(i, ft_range(d - mlx->l, d - mlx->k), ft_range(255, 250)),
+				ft_map(i, ft_range(d - mlx->l, d - mlx->k), ft_range(50, 40)));
+	if (i < d - mlx->l)
+		color = ft_encode_argb(255,
+				ft_map(i, ft_range(0, d - mlx->l), ft_range(10, 30)),
+				ft_map(i, ft_range(0, d - mlx->l), ft_range(0, 255)),
+				ft_map(i, ft_range(0, d - mlx->l), ft_range(10, 50)));
 	return (color);
 }
 
